67e4eee2b8fbaef550b11eb679d0e52b9f872bbf
JosueOb <josuericardocando@hotmail.com>
2021-05-10 00:29:49 -0500

    Extensión de la paleta de colores y fuente Nunito, y se agregó el diseño del logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        custom : ['Nunito', 'sans-serif'],
+      },
+      colors: {
+        'primary-blue': '#4A6572',
+        'secondary-blue': '#344955',
+        'tertiary-blue': '#232F34',
+        'primary-orange': '#F9AA33',
+        'dark-background': '#F9AA33'
+      }
+    },
   },
   variants: {
     extend: {},

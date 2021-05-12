52d8dd9370bc36606ce6707da32bbf3ae8ffe4cb
JosueOb <josuericardocando@hotmail.com>
2021-05-12 14:59:55 -0500

    Selección del tema según el sistema por defecto o manualmente

---
+++ docs/js/theme_mode.js
@@ -0,0 +1,48 @@
+function ThemeMode() {
+    return {
+        dark: false,
+        setTheme: function () {
+            var htmlSelector = document.querySelector('html').classList;
+            if ('theme' in localStorage) {
+                if (localStorage.theme == 'dark') {
+                    // Whenever the user explicitly chooses light mode
+                    htmlSelector.remove('dark');
+                    localStorage.theme = 'light'
+                    this.dark = false;
+                } else {
+                    // Whenever the user explicitly chooses dark mode
+                    htmlSelector.add('dark');
+                    localStorage.theme = 'dark'
+                    this.dark = true;
+                }
+                
+            } else {
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    htmlSelector.remove('dark');
+                    localStorage.theme = 'light'
+                    this.dark = false;
+                } else {
+                    htmlSelector.add('dark');
+                    localStorage.theme = 'dark'
+                    this.dark = true;
+                }
+            }
+
+        },
+        setIcon: function () {
+            if ('theme' in localStorage) {
+                if (localStorage.theme === 'dark') {
+                    this.dark = true;
+                } else {
+                    this.dark = false;
+                }
+            } else {
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    this.dark = true;
+                } else {
+                    this.dark = false;
+                }
+            }
+        }
+    }
+}

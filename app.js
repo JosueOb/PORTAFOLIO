6aa364738a2a9cb755fb6a028b0d1353b2aa062c
JosueOb <josuericardocando@hotmail.com>
2021-05-12 00:44:28 -0500

    Personalización del menú de navegación y aplicación del modo oscuro

--- app.js
+++
@@ -1,3 +0,0 @@
-import "./css/tailwind.css";
-import "alpinejs";
-import "bootstrap-icons/font/bootstrap-icons.css";
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import "../css/tailwind.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "alpinejs";

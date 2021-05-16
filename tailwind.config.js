0594286d7f5973aa8906fcc8bbf71158d80a5dd7
JosueOb <josuericardocando@hotmail.com>
2021-05-16 01:50:21 -0500

    Corrección en los estilos del modo oscuro y claro del navbar y hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,10 @@ module.exports = {
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         custom : ['Nunito', 'sans-serif'],
       },
       colors: {
-        'primary-blue': '#4A6572',
-        'secondary-blue': '#344955',
-        'tertiary-blue': '#232F34',
         'primary-orange': '#F9AA33',
         'secondary-orange': '#ffd942',
         'tertiary-orange': '#fdeb5d',
@@ -17,7 +14,14 @@ module.exports = {
         'primary-dark': '#121212',
         'secondary-dark': '#1e1e1e',
         'tertiary-dark': '#212121',
-        'quarternary-dark': '#272727'
+        'quarternary-dark': '#272727',
+        //ligth mode
+        'light-surface': '#ffff',
+        'light-backgound': '#f3f4f6',
+        //dark mode
+        'dark-surface': '#000000',
+        'dark-background': '#121212',
+
       }
     },
   },
